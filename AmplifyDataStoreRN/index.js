8840c2b574afabec9b4f00c5c005c5c376a781d4
David McAfee <mcafd@amazon.com>
2023-03-16 15:10:55 -0700

    fix build

--- AmplifyDataStoreRN/index.js
+++ AmplifyDataStoreRN/index.js
@@ -2,15 +2,15 @@
  * @format
  */
 
+// import 'core-js/full/symbol/async-iterator';
+// import 'react-native-get-random-values';
+// import 'react-native-url-polyfill';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 import {DataStore} from 'aws-amplify';
 import {SQLiteAdapter} from '@aws-amplify/datastore-storage-adapter/SQLiteAdapter';
-import 'core-js/full/symbol/async-iterator';
-import 'react-native-get-random-values';
-import 'react-native-url-polyfill';
 
 DataStore.configure({
   storageAdapter: SQLiteAdapter,
